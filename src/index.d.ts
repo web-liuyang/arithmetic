b8fdb22b43fc44ada4f0f2de4906c0739d35f4ef
web-liuyang <984584014@qq.com>
2021-04-02 15:58:07 +0800

    update ok

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,8 @@
 export function sm3(str: string): string;
 
-export type sm4 = {
+type sm4 = {
   encrypt: (str: string, key: string) => string;
   decrypt: (str: string, key: string) => string;
 };
+
+export const sm4: sm4;
